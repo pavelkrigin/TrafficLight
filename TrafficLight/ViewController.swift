44cb7b5e6dcc76415f9bdbeda63fa40630fb2af8
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-07-29 22:20:13 +0300

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Pavel Krigin on 29.7.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
